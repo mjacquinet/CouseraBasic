4a9e340a701c03549964239a11d433873d822240
upjohnc <upjohnc@gmail.com>
2013-01-12 12:10:05 -0600

    Submission of first test

--- CouseraBasic/getmonitor.R
+++ CouseraBasic/getmonitor.R
@@ -42,3 +42,6 @@ getmonitor <- function(id, directory, summarize = FALSE) {
   return(data)
   
 }
+
+source("http://spark-public.s3.amazonaws.com/compdata/scripts/getmonitor-test.R")
+getmonitor.testscript()
