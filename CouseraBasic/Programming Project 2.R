8b6e3754e8b18b75fbeb1974e181f72a8fa19c2e
upjohnc <upjohnc@gmail.com>
2013-01-13 13:12:35 -0600

    Including corr function with Project 2

--- CouseraBasic/Programming Project 2.R
+++ CouseraBasic/Programming Project 2.R
@@ -75,3 +75,29 @@ complete <- function(directory, id = 1:332) {
   
   return(completeDf)
 }
+
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  
+  corData <- vector(mode="integer")
+  
+  fileCount<-length(list.files(directory, pattern = "\\.csv$"))
+  
+  for(idCounter in 1:fileCount){
+    tempData<-getmonitor(idCounter, directory)
+    complete(directory= directory, id = idCounter)
+    if(!is.na(complete(directory= directory, id = idCounter)[,"nobs"]) && complete(directory= directory, id = idCounter)[,"nobs"]> threshold){
+      corData<- c(corData,cor(tempData[,"sulfate"], tempData[,"nitrate"], use="pairwise.complete.obs"))
+    }
+  }
+  
+  return(corData)
+} 
