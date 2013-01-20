9e3b46ae74a1a830bfcdc045855cb2261c17799f
upjohnc <upjohnc@gmail.com>
2013-01-20 12:28:39 -0600

    Finished Part 3

--- Program 3/Summary 3.R
+++ Program 3/Summary 3.R
@@ -28,12 +28,19 @@ abline(v = median(outcome[,23], na.rm=TRUE), untf = FALSE, col = "red")
 tempState <- data.frame(table(outcome$State))
 temp<-subset(outcome, table(outcome$State) > 20)
 tempState<-subset(tempState, Freq > 20, select = Var1)
+tempState<- sapply(tempState, as.character)
 #forced the subset of states to be a character
-subsetStates<-c("AL","AR","AZ","CA","CO","CT","FL","GA","IA","ID","IL","IN","KS","KY","LA", "MA"
-                ,"MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH"
-                ,"OK","OR","PA","PR","SC","SD","TN","TX","UT","VA","WA","WI","WV","WY")
-
-outcome2 <- subset(outcome, State %in% subsetStates)
+##subsetStates<-c("AL","AR","AZ","CA","CO","CT","FL","GA","IA","ID","IL","IN","KS","KY","LA", "MA"
+##                ,"MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH"
+##                ,"OK","OR","PA","PR","SC","SD","TN","TX","UT","VA","WA","WI","WV","WY")
 
 
-death
+outcome2 <- subset(outcome, State %in% tempState)
+
+
+death<-outcome2[,11]
+state<-outcome2$State
+par(mfrow = c(1,1))
+boxplot(death ~ state, ylab="30-day Death Rate", main="Heart Attack 30-day Death Rate by State")
+
+#Part 4
